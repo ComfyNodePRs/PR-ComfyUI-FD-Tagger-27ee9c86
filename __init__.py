66f1ab31678a4f0a6312c3da91539751d3363e1e
loopyd <nightwintertooth@gmail.com>
2024-07-18 02:32:09 -0700

    Fix requirements

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .comfynode import init
 
-if init(check_imports=["pytorch", "torchvision", 'PIL']):
+if init(check_imports=["torch", "torchvision", "pillow", "aiohttp", "requests", "numpy"]):
     from .fdtagger import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
     WEB_DIRECTORY = "./web"
     __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
