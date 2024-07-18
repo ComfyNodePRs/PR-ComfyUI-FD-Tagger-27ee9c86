99b5f3cdcaa35633e197abe25de36431a7e11d8c
loopyd <nightwintertooth@gmail.com>
2024-07-18 02:33:26 -0700

    pillow -> PIL

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .comfynode import init
 
-if init(check_imports=["torch", "torchvision", "pillow", "aiohttp", "requests", "numpy"]):
+if init(check_imports=["torch", "torchvision", "PIL", "aiohttp", "requests", "numpy"]):
     from .fdtagger import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
     WEB_DIRECTORY = "./web"
     __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
