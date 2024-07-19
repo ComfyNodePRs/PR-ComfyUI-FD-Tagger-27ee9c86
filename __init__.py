f8d3dc099accc7830c6020a438c8be894aa07370
loopyd <nightwintertooth@gmail.com>
2024-07-19 15:40:48 -0700

    changed

--- __init__.py
+++ __init__.py
@@ -2,7 +2,10 @@ import os
 from .helpers.extension import ComfyExtension
 from .helpers.nodes import ComfyNode
 
-if init(check_imports=["torch", "torchvision", "PIL", "aiohttp", "requests", "numpy"]):
-    from .fdtagger import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-    WEB_DIRECTORY = "./web"
-    __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+if not ComfyExtension().init(check_imports=["torch", "torchvision", "PIL", "aiohttp", "requests", "numpy"]):
+    raise ImportError("ComfyExtension failed to initialize")
+
+vars = ComfyNode().get_node_vars(os.path.dirname(__file__))    
+for name, obj in vars.items():
+    globals()[name] = obj
+__all__ = list(vars.keys())
