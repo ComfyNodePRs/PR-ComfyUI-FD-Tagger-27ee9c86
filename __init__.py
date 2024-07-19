e9ad834998d7fbe75b2cea0fa53a8c1b67746f1c
loopyd <nightwintertooth@gmail.com>
2024-07-19 15:26:33 -0700

    update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
-from .comfynode import init
+import os
+from .helpers.extension import ComfyExtension
+from .helpers.nodes import ComfyNode
 
-if init(check_imports=["pytorch", "torchvision", 'PIL']):
+if init(check_imports=["torch", "torchvision", "PIL", "aiohttp", "requests", "numpy"]):
     from .fdtagger import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
     WEB_DIRECTORY = "./web"
     __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
